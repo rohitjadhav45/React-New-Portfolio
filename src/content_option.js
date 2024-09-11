d969a21834a37194a56d5a5789d48dc5b33d4a52
PravinSargar <pravinsargar2001@gmail.com>
2024-09-11 22:40:48 +0530

    update

---
+++ src/content_option.js
@@ -0,0 +1,110 @@
+const logotext = "ROHIT";
+const meta = {
+    title: "Rohit Jadhav",
+    description: "I’m Rohit Jadhav React Js devloper",
+};
+
+const introdata = {
+    title: "I’m Rohit Jadhav",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "At Microdynamic Software, we are passionate about creating seamless and interactive web experiences using the latest in frontend technology. With a focus on React.js, we specialize in developing high-performance, scalable, and user-friendly applications that cater to diverse business needs.",
+    your_img_url: "https://cdn.wallpapersafari.com/4/92/5vSW6p.png",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "At Microdynamic Software, we are passionate about creating seamless and interactive web experiences using the latest in frontend technology. With a focus on React.js, we specialize in developing high-performance, scalable, and user-friendly applications that cater to diverse business needs.",
+};
+const worktimeline = [{
+        jobtitle: "React js",
+        where: "Pune",
+        date: "2022",
+    },
+];
+
+const skills = [{
+        name: "Html",
+        value: 95,
+    },
+    {
+        name: "CSS",
+        value: 95,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 90,
+    },
+    {
+        name: "Bootstrap",
+        value: 90,
+    },
+];
+
+const services = [{
+        title: "Custom Web Application Development",
+        description: "Building dynamic and responsive web applications using React.js to deliver seamless user experiences.",
+    },
+    {
+        title: "E-commerce Application Development",
+        description: "Developing e-commerce platforms with React and React Native for both web and mobile, tailored to meet specific business needs.",
+    },
+    {
+        title: "Single Page Applications (SPA)",
+        description: "Designing and developing fast, scalable SPAs with React, reducing load times and enhancing the user journey.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Decorsaga Design",
+        link: "https://www.decorsagadesign.com/",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Goods24",
+        link: "https://goods24website.microdynamicsoftware.uk/",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Microdynamic Software",
+        link: "https://www.microdynamicsoftware.com/",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "jadhavrp4510@gmail.com",
+    YOUR_FONE: "9022609521",
+    description: "At Microdynamic Software, we are passionate about creating seamless and interactive web experiences using the latest in frontend technology. With a focus on React.js, we specialize in developing high-performance, scalable, and user-friendly applications that cater to diverse business needs.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/rohit-jadhav-845126229?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
